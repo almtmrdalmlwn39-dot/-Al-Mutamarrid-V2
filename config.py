7e951f81f40196919e6b6f617275f67f7a231e0a
almtmrdalmlwn39-dot <almtmrdalmlwn39@gmail.com>
2026-05-27 15:34:04 +0300

    Create config.py with bot settings and developer info
    Add configuration settings for the bot including API credentials and developer information.

---
+++ config.py
@@ -0,0 +1,14 @@
+# --- إعدادات سورس المتمرد V2 النهائية ---
+
+API_ID = 26588241
+API_HASH = "b90956461a510523097f48030999554b"
+BOT_TOKEN = "7295980036:AAGR8wQYmX_uS_4-YF2D6tQpB2V_m_e-S_A"
+
+# قائمة المطورين (الحسابين)
+SUDO_USERS = [7447817025, 6467728995] 
+
+# يوزرات المطورين للحقوق
+SUDO_USERNAMES = ["@A0_O7", "@Vi_ti0"]
+
+# روابط القنوات
+CHANNEL_LINK = "https://t.me/bedmoddinnow"
