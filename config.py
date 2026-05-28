2ca0bce463e3104a6dd5410a689a8bbf14b6a5c8
almtmrdalmlwn39-dot <almtmrdalmlwn39@gmail.com>
2026-05-28 15:33:16 +0300

    Modify SUDO_USERS in config.py
    Updated SUDO_USERS list with a new user ID.

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_HASH = "184e7fd176413cd0d2425494f1796229"
 BOT_TOKEN = "8762367853:AAH5769kMPrIrvTGr1aOTYnUwF2nfvzcp4I" # التوكن الصحيح هنا فقط
 
 # قائمة المطورين
-SUDO_USERS = [7447817025, 6467728995] 
+SUDO_USERS = [6895436017, 6467728995] 
 SUDO_USERNAMES = ["@A0_O7", "@Vi_ti0"]
 # قناة البوت
 CHANNEL_LINK = "https://t.me/bedmoddinnow"
