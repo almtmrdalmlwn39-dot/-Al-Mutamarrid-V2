6092d1f13948b1f31482f4e9ab7fd261644b5ac5
almtmrdalmlwn39-dot <almtmrdalmlwn39@gmail.com>
2026-05-28 13:32:59 +0300

    Update BOT_TOKEN and clean up comments in config.py

--- config.py
+++ config.py
@@ -2,13 +2,9 @@
 
 API_ID = 22610186
 API_HASH = "184e7fd176413cd0d2425494f1796229"
-BOT_TOKEN = "7295980036:AAGR8wQYmX_uS_4-YF2D6tQpB2V_m_e-S_A"
+BOT_TOKEN = "8762367853:AAFoyDjc55d0fPTvRThmDFh8_wsQY39Br9g" # التوكن الصحيح هنا فقط
 
-# قائمة المطورين (الحسابين)
+# قائمة المطورين
 SUDO_USERS = [7447817025, 6467728995] 
-
-# يوزرات المطورين للحقوق
 SUDO_USERNAMES = ["@A0_O7", "@Vi_ti0"]
-
-# روابط القنوات
 CHANNEL_LINK = "https://t.me/bedmoddinnow"
